483d765c853281fc5510cdb4b9197b3debaccd86
Pablo García Fernández <uo276824@uniovi.es>
2026-05-29 11:32:04 +0200

    subtask2

--- task.py
+++ task.py
@@ -9,7 +9,9 @@ def prodotto_scalare(v1: list, v2: list) -> float:
 
 def rango_matrice(m: list) -> int:
     """Sub-task 2: Calcola il rango di una matrice."""
-    pass
+    A = np.array(m)
+    int=np.linalg.matrix_rank(A)
+    return int
 
 def risolvi_sistema_lineare(A: list, b: list) -> np.ndarray:
     """Sub-task 3: Risolvere un Sistema Lineare."""
