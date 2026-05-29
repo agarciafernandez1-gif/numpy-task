a62db5d185258ce9fd954cfbd1341f2e91328227
Pablo García Fernández <uo276824@uniovi.es>
2026-05-29 11:36:43 +0200

    subtask3

--- task.py
+++ task.py
@@ -15,7 +15,8 @@ def rango_matrice(m: list) -> int:
 
 def risolvi_sistema_lineare(A: list, b: list) -> np.ndarray:
     """Sub-task 3: Risolvere un Sistema Lineare."""
-    pass
+    x = np.linalg.solve(A, b)
+    return x
 
 def correlazione_matrici(m1: list, m2: list) -> np.ndarray:
     """Sub-task 4: Correlazione tra Matrici 2x2."""
